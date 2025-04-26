ca8a1899a63b2171564befff4cdb24f21a9365b2
romullo-pereira <romullop21@gmail.com>
2025-04-26 19:04:19 -0300

    Adicionando validação de usuario e testes unitarios

--- src/main/kotlin/com/romullo/pereira/expensensecontrol/domain/model/login/LoginRequest.kt
+++ src/main/kotlin/com/romullo/pereira/expensensecontrol/domain/model/login/LoginRequest.kt
@@ -1,6 +1,16 @@
 package com.romullo.pereira.expensensecontrol.domain.model.login
 
+import com.romullo.pereira.expensensecontrol.domain.commons.DefaultMessages.INVALID_EMAIL
+import com.romullo.pereira.expensensecontrol.domain.commons.isValidEmail
+
 data class LoginRequest(
-        val email: String,
-        val password: String
-)
+    val email: String,
+    val password: String,
+) {
+    fun validate(): LoginRequest {
+        if (email.isNotBlank() && !email.isValidEmail()) {
+            throw IllegalArgumentException(INVALID_EMAIL)
+        }
+        return this
+    }
+}
