017adb96855a7f0e3048ba6880e4fac344ffde39
romullo-pereira <romullop21@gmail.com>
2026-03-15 11:47:12 -0300

    feat: define inbound and outbound port interfaces (task 3)

--- src/main/kotlin/com/romullo/pereira/expensensecontrol/domain/port/inbound/ListCategoriesUseCase.kt
+++ src/main/kotlin/com/romullo/pereira/expensensecontrol/domain/port/inbound/ListCategoriesUseCase.kt
@@ -1,5 +1,7 @@
 package com.romullo.pereira.expensensecontrol.domain.port.inbound
 
+import com.romullo.pereira.expensensecontrol.domain.model.category.CategoryResponse
+
 interface ListCategoriesUseCase {
-    fun listByUser(userId: String): List<String>
+    fun listByUser(userId: String): List<CategoryResponse>
 }
